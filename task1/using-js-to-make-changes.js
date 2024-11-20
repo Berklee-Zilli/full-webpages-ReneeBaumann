2e3b1f0b1c5dfe311ca6dbbfa2377439ae69362f
Renée Baumann <reneebaumann@Renees-MacBook-Pro.local>
2024-11-19 19:23:42 -0500

    LEvel 1 complete

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,12 @@
+document.title = "TASK 1 by Renée";
+
+document.querySelector("h1").textContent = "TASK 1"
+
+document.querySelector("h2").textContent = "Things I like"
+
+document.getElementById("intro").textContent = "I am a berklee student"
+
+
+
+
+
